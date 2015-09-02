ee23810d1837b8193be1cdfb2a5489df627744e4
Sergey Romanov <xxsmotur@gmail.com>
2015-09-03 02:24:01 +0500

    first commit

---
+++ lampartclock.go
@@ -0,0 +1,28 @@
+package lampartclock
+
+import
+(
+	"sync"
+	"sync/atomic"
+)
+
+type LampartClock {
+	counter uint64
+}
+
+//Inc provides increment counter
+func (lc *LampartClock) Inc() {
+	atomic.AddUint64(&lc.counter, 1)
+}
+
+//Get provides current counter
+func (lc *LampartClock) Get() int64 {
+	return atomic.LoadUint64(&lc.counter)
+}
+
+func (lc *LampartClock) Set(num uint64) {
+	value := lc.Get()
+	if num < value {
+		return 
+	}
+}
