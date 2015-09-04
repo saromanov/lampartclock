ef66ebedadd0dd3cd92da9dfe725eca9828ea710
Sergey Romanov <xxsmotur@gmail.com>
2015-09-04 23:22:29 +0500

    Change SetTo to Sync

--- lampart.go
+++ lampart.go
@@ -10,17 +10,20 @@ type Lampart struct {
 	nodes map[string]*LampartClock
 }
 
+//New provides creating a new object
 func New()*Lampart {
 	lam := new(Lampart)
 	lam.nodes = map[string]*LampartClock{}
 	return lam
 }
 
+//Add provides store name of node
 func (lam *Lampart) Add(title string) {
 	lam.nodes[title] = new(LampartClock)
 }
 
-func (lam *Lampart) Send(title string)(error) {
+//Send provides increment counter of node with title
+func (lam *Lampart) Send(title string)error {
 	clock, ok := lam.nodes[title]
 	if !ok {
 		return errors.New(fmt.Sprintf("Node with name %s is not found", title))
@@ -29,6 +32,22 @@ func (lam *Lampart) Send(title string)(error) {
 	return nil
 }
 
+//SenTo provides synchronization between two clocks
+func (lam *Lampart) Sync(titlefrom string, titleto string) error {
+	clock1, ok := lam.nodes[titlefrom]
+	if !ok {
+		return errors.New(fmt.Sprintf("Node with name %s is not found", titlefrom))
+	}
+	clock2, ok2 := lam.nodes[titleto]
+	if !ok2 {
+		return errors.New(fmt.Sprintf("Node with name %s is not found", titleto))
+	}
+
+	clock2.Set(clock1.Get())
+	return nil
+}
+
+//GetCounter provides getting of counter from node with title
 func (lam *Lampart) GetCounter(title string) (uint64, error) {
 	clock, ok := lam.nodes[title]
 	if !ok {
